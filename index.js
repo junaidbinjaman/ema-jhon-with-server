c98e31c5a121852763e58eacbafdfe30d0bf9177
jwol65869 <junaid65859@gmail.com>
2021-03-29 15:16:01 +0600

    MY SERVER cODE

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.raxaw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+const port = 4000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db('emaJhonStore').collection('products');
+    const orderCollection = client.db('emaJhonStore').collection('orders');
+
+    app.post('/addProduct', (req, res) => {
+        const produts = req.body;
+        productsCollection.insertOne(produts)
+            .then(result => {
+                console.log(result.insertedCount)
+                res.send(result.insertedCount);
+            })
+    })
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
+    })
+
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        productsCollection.find({ key: { $in: productKeys } })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        orderCollection.insertOne(order)
+            .then(result => {
+                console.log(result.insertedCount)
+                res.send(result.insertedCount > 0);
+            })
+    })
+});
+
+
+
+app.listen(port)
